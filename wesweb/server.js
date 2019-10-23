f42268bae2f36fa8f848e59e98b16f437b783b3b
Emil <emil.delrosso@elev.ga.ntig.se>
2019-10-23 14:00:39 +0200

    vadim

--- wesweb/server.js
+++ wesweb/server.js
@@ -1,6 +1,18 @@
-var http = require('http');
+const {createServer} = require("http");
+const {createReadStream} = require("fs");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+const sendFile = (response, status, type, filepath) => {
+    response.writeHead(status, {"Content-Type": type});
+    createReadStream(filepath).pipe(response);
+};
+
+createServer((request, response) => {
+    switch (request.url) {
+        case "/":
+            return sendFile(response, 200, "text/html", "./CSS 3 test.html");
+        case "/CSS%203%20test.css":
+            return sendFile(response, 200, "text/css", "./CSS 3 test.css");
+        case "/dsIII.jpg":
+            return sendFile(response, 200, "img/jpg", "./dsIII.jpg");
+    }
 }).listen(8080);
